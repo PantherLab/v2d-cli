acb1ffec2ab0d9b5e913e83dd14aa21038eda145
José Ignacio Escribano <jiep12345@hotmail.com>
2018-04-24 11:41:38 +0200

    Fixed error

--- v2d/main.py
+++ v2d/main.py
@@ -92,7 +92,7 @@ def main():
             print('Similar domains to {}'.format(dom))
             domains.difference_update(set(dom))
             for d in domains:
-                print_diff(args.domain, d)
+                print_diff(dom, d)
                 if write:
                     f.write(d + "\n")
             if (args.check):
