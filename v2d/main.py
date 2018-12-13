e0eaeaff0f9cc69d49bfc5f5b5255ccdb63a4cba
Miguel000 <miguelhernandez2907@gmail.com>
2018-12-13 17:30:45 +0100

    add new version v2d, substitution and flipper

--- v2d/main.py
+++ v2d/main.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import signal
+from tld import get_tld
 
 from .domains import similar_domains, check_domains
 from .utils.similarity import load_file
@@ -11,6 +12,53 @@ DESCRIPTION = ('v2d-cli: Visual Unicode attacks with Deep Learning - '
                'means of Deep Learning. This provides a greater number of '
                'variations and a possible update over time')
 
+def generate_flipper_domains(dom):
+
+    url = get_tld(dom, as_object=True, fix_protocol=True)
+    domain = url.domain
+    new_urls_without_letter = []
+    n = 0
+    m = len(domain)
+
+    if m == 1:
+        new_urls_without_letter.append(domain)
+    elif m == 2:
+        new_domain = domain[1] + domain[0]
+        new_urls_without_letter.append(new_domain)
+
+    else:
+
+        while n < m and m > 2:
+
+            if n == 0 :
+                new_domain = domain[n + 1] + domain[n] + domain[n + 2:m]
+
+            elif n == 1:
+                new_domain = domain[0] + domain[n + 1] + domain[n] + domain[n + 2:m]
+
+            elif 1 < n < m - 1:
+                new_domain = domain[0:n] + domain[n + 1] + domain[n] + domain[n + 2:m]
+
+            n = n + 1
+            new_urls_without_letter.append(new_domain+"."+url.tld)
+    new_urls_list = list(set(new_urls_without_letter))
+    return new_urls_list
+
+def generate_substitution_domains(dom):
+    url = get_tld(dom, as_object=True, fix_protocol=True)
+    domain = url.domain
+
+    new_urls_with_double_letter = []
+    n = 0
+    m = len(domain)
+    while n < m:
+        new_domain = domain[0:n] + domain[n] + domain[n] + domain[n+1:m]
+        new_urls_with_double_letter.append(new_domain+"."+url.tld)
+        new_domain = domain[0:n] + domain[n+1:m]
+        new_urls_with_double_letter.append(new_domain+"."+url.tld)
+        n = n + 1
+    new_urls_list = list(set(new_urls_with_double_letter))
+    return new_urls_list
 
 def banner():
     print("""
@@ -39,8 +87,14 @@ def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('-d', '--domain', action='store',
+    parser.add_argument('domain', action='store',
                         help='check similar domains to this one')
+
+    parser.add_argument('-F','--flipper', dest='flipper',default=False, nargs='?', const=True, type=bool, help='Execute flipping attack')
+    parser.add_argument('-H','--homoglyph', dest='homoglyph',default=False, nargs='?',const=True, type=bool,help="Execute homoglyph attack")
+    parser.add_argument('-S','--substitution', dest='substitution',default=False, nargs='?', const=True, type=bool, help="Execute substitution attack")
+
+
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-c', '--check', action='store_true',
                         help='check if this domain is alive')
@@ -74,9 +128,12 @@ def main():
         print('Please, enter a VirusTotal API Key with -api or --api-key')
         sys.exit(-1)
 
-    confusables = load_file(args.threshold)
-
+    if not (args.homoglyph or args.substitution or args.flipper):
+        print("Need one type of attack, -F -Hg or -S")
+        print()
+        sys.exit(-1)
     idomains = list()
+    #domains = list()
     write = False
     if args.fileinput:
         try:
@@ -94,13 +151,32 @@ def main():
     if (args.output):
         f = open(args.output, 'w')
         write = True
+
+    confusables = load_file(args.threshold)
+
     for dom in idomains:
-        domains = set(similar_domains(dom, confusables, args.max))
+
+        if args.homoglyph:
+            print('Option selected: Homoglyph attack with a {}% threshold.'.format(args.threshold))
+            print('Domain target: ' + dom)
+            domains = set(similar_domains(dom, confusables, args.max))
+        if args.flipper:
+            print('Option selected: Flipping attack')
+            print('Domain target: ' + dom)
+            domains = set(generate_flipper_domains(dom))
+        if args.substitution:
+            print('Option selected: Substitution attack')
+            print('Domain target: ' + dom)
+            domains = set(generate_substitution_domains(dom))
         if len(domains) > 0:
             print('Similar domains to {}'.format(dom))
-            domains.difference_update(set(dom))
+            if len(domains) > 0:
+                domains.difference_update(set(dom))
             for d in domains:
-                print_diff(dom, d)
+                if not args.substitution:
+                    print_diff(dom, d)
+                else:
+                    print (d)
                 if write:
                     f.write(d + "\n")
             if (args.check):
@@ -111,9 +187,9 @@ def main():
                               whois=args.whois,
                               vt=args.virustotal)
             print('Total similar domains to {}: {}'.format(dom, len(domains)))
+
     if write:
         f.close()
-
 
 if __name__ == 'main':
     main()
